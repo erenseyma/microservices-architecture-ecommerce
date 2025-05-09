b56f3397a7fd798884ccbca74a8b4c92c7ddb6a0
erenseyma <erenn.seyma@gmail.com>
2025-05-09 16:54:43 +0300

    first commit

---
+++ Services/Catalog/ECommerce.Catalog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ECommerce.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; } //mongo da ilişkili olmadığı için int değl string kullnadık
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Services/Catalog/ECommerce.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        public string ProductDetailId { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+    }
+}
